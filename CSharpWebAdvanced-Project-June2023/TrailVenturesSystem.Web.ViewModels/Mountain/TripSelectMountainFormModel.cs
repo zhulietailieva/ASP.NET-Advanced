8072a1ff7cb0ac93c213d4d26a7a01344d58e057
Zhulieta Ilieva <iilievazhulieta@gmail.com>
2023-07-29 02:27:39 +0300

    Created Add Trip form model and added validations for adding a new trip.

---
+++ CSharpWebAdvanced-Project-June2023/TrailVenturesSystem.Web.ViewModels/Mountain/TripSelectMountainFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrailVenturesSystem.Web.ViewModels.Mountain
+{
+    //no need for validations because we get this model from the db and load it to the form
+    public class TripSelectMountainFormModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- CSharpWebAdvanced-Project-June2023/TrailVenturesSystem.Web.ViewModels/Trip/TripFormModel.cs
+++ CSharpWebAdvanced-Project-June2023/TrailVenturesSystem.Web.ViewModels/Trip/TripFormModel.cs
@@ -1,8 +1,40 @@
 ﻿namespace TrailVenturesSystem.Web.ViewModels.Trip
 {
+    using System.ComponentModel.DataAnnotations;
+    using TrailVenturesSystem.Web.ViewModels.Mountain;
+
+    using static Common.EntityValidationConstants.Trip;
+
     public class TripFormModel
     {
+        public TripFormModel()
+        {
+            this.Mountains = new HashSet<TripSelectMountainFormModel>();
+        }
+
+        [Required]
+        [StringLength(TitleMaxLength,MinimumLength =TitleMinLength)]
         public string Title { get; set; } = null!;
 
+        [Required]
+        [Display(Name ="Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Return Date")]
+        public DateTime ReturnDate { get; set; }
+
+        [Required]
+        [StringLength(DescriptionMinLength,MinimumLength =DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+
+        [Display(Name = "Price Per Person")]
+        [Range(typeof(decimal),PricePerPersonMinValue,PricePerPersonMaxValue)]
+        public decimal PricePerPerson { get; set; }
+         
+        //selected mountain by user
+        public int MountainId { get; set; }
+        public IEnumerable<TripSelectMountainFormModel> Mountains { get; set; }
     }
 }
